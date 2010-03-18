4469b26d20c02ed92d7dbabc668c8adfd287d663
falcon <falcon@hawk.home.oxos.pl>
2010-03-18 15:45:30 +0100

    gcarousel.js code refactor

--- gcarousel.js
+++ gcarousel.js
@@ -9,17 +9,12 @@
 
         var this_ = this;
 
-        // carousel elements
-        this.carousel_wrapper = null;
         this.items_container = $(element);
+        this.carousel_wrapper = null;
         this.next_button = null;
         this.prev_button = null;                
-
-        // moving parameters
         this.current_left = 0;
-
-        // other attributes
-        this.items_count = null;
+        this.items_count = 0;
 
         this.defaults = {
             number_slides_visible: "1",
@@ -32,70 +27,45 @@
         
         $.extend(this.defaults, options);
 
-        // methods -----------------------------------------------------
-        this.initialize = function(){
-            
-            if (this_.defaults.auto_adjust)
-                this_.auto_adjust_carousel();
-            else
-                this_.basic_carousel();
+        initialize = function(){
+            this_.items_container.css("position","absolute");
+            this_.items_count = this_.items_container.children().length;
 
-            this_.setup_carousel();
-
-            // add events for buttons
-            if (this_.next_button)
-                this_.next_button.click(function() {
-                    this_.skip('next');
-                });
-            if (this_.prev_button)
-                this_.prev_button.click(function() {
-                    this_.skip('prev');
-                });
+            if (this_.defaults.auto_adjust)   
+                auto_adjust_carousel();
         };
 
+        this.skip = function(direction) {
+            var wrapper_width = this_.items_container.width();
+
+            // calculate current_left
+            if (direction == "next")
+                this_.current_left -= this_.defaults.shift;
+            if (direction == "prev")
+                this_.current_left += this_.defaults.shift;
+
+            if (this_.current_left + (wrapper_width-this_.defaults.shift) < 0) this_.current_left = 0;
+            if (this_.current_left > 0) this_.current_left = -(wrapper_width-this_.defaults.shift);
+
+            // animation
+            this_.carousel_transition(this_.current_left);
+        };
+
+
         this.carousel_transition = function(position) {
-                this_.items_container.stop().animate({
-                    "left": position
-                },
-                {
-                    "duration": this_.defaults.transition_duration,
-                    "easing": this_.defaults.transition_easing
-                });
-            };
-
-
-        // basic carousel constructor ------------------------------------------
-        this.basic_carousel = function() {
-
-            this_.setup_carousel = function(){
-                this.items_container.css("position","absolute");
-                this_.items_count = this_.items_container.children().length;
-            };
-
-            this_.skip = function(direction) {
-
-                var wrapper_width = this_.items_container.width();
-
-                // calculate current_left
-                if (direction == "next")
-                    this_.current_left -= this_.defaults.shift;
-                if (direction == "prev")
-                    this_.current_left += this_.defaults.shift;
-
-                if (this_.current_left + (wrapper_width-this_.defaults.shift) < 0) this_.current_left = 0;
-                if (this_.current_left > 0) this_.current_left = -(wrapper_width-this_.defaults.shift);
-
-                // animation
-                this_.carousel_transition( this_.current_left );
-
-            }; 
-
+            this_.items_container.stop().animate({
+                "left": position
+            },
+            {
+                "duration": this_.defaults.transition_duration,
+                "easing": this_.defaults.transition_easing
+            });
         };
 
         // auto adjust carousel constructor ------------------------------------
-        this.auto_adjust_carousel = function() {
+        auto_adjust_carousel = function() {
 
-            this_.setup_carousel = function(){
+            setup_wrappers = function(){
                 // add class to scrolled div
                 this_.items_container.addClass("row_of_slides");
 
@@ -114,42 +84,38 @@
                 this_.next_button = this_.carousel_wrapper.children(".next_button");
 
                 this_.items_count = this_.items_container.children().length;
+            };
 
-                // setup items wrapper and items
-                var wrapper_width = this_.items_container.width();
-                var item_width = Math.floor(this_.items_container.parent().width() / this_.defaults.number_slides_visible);
-                var all_slides_width = Math.floor(this_.items_count * item_width);
-
+            setup_items = function(){
                 this_.items_container.css('width', all_slides_width);
                 this_.items_container.children().css("width", item_width+"px");
                 this_.items_container.children().addClass("slide");
             };
-                                                
-            this_.skip = function(direction) {
-                var wrapper_width = this_.items_container.width();
-                var item_width = Math.floor(this_.items_container.parent().width() / this_.defaults.number_slides_visible);
-                var offset = this_.defaults.scroll * item_width;
 
-                // calculate current_left
-                if (direction == "next")
-                    this_.current_left -= offset;
-                if (direction == "prev")
-                    this_.current_left += offset;
-                
+            setup_wrappers();
+            
+            // setup items
+            var wrapper_width = this_.items_container.width();
+            var item_width = Math.floor(this_.items_container.parent().width() / this_.defaults.number_slides_visible);
+            var all_slides_width = Math.floor(this_.items_count * item_width);
+            
+            setup_items();
 
-                if (this_.current_left + (wrapper_width-this_.defaults.shift) < 0) this_.current_left = 0;
-                if (this_.current_left > 0) this_.current_left = -(wrapper_width-offset);
+            this_.defaults.shift =  this_.defaults.scroll * item_width;
 
-                // animation
-                this_.carousel_transition( this_.current_left );
+            // add events for buttons
+            this_.next_button.click(function() {
+                this_.skip('next');
+            });
 
-            };
+            this_.prev_button.click(function() {
+                this_.skip('prev');
+            });
 
         } 
 
-
         // constructor
-        this.initialize();
+        initialize();
 
     } // function agile_carousel
 
