cb1288f231318576d90758e9bb979861ac14770d
Wojciech Kruszewski <wojciech@oxos.pl>
2010-03-22 12:05:34 +0100

    Use this

--- gcarousel.js
+++ gcarousel.js
@@ -7,104 +7,111 @@
 
     $.gcarousel = function(element, input_options) {
 
-        var carousel = this;
-        carousel.items_container = $(element);
+	this.initialize = function() {
+        	this.items_container = $(element);
 
-        carousel.options = {
-            number_slides_visible: "1",
-            transition_duration: 1000,
-            transition_easing: "swing",
-            scroll: 1,
-            auto_adjust: false,
-            shift: 100
-        };
-        $.extend(this.options, input_options);
+        	this.options = {
+        	    number_slides_visible: "1",
+        	    transition_duration: 1000,
+        	    transition_easing: "swing",
+        	    scroll: 1,
+        	    auto_adjust: false,
+        	    shift: 100
+        	};
+        	$.extend(this.options, input_options);
 
-        carousel.items_container_width = carousel.items_container.width();
+        	this.items_container_width = this.items_container.width();
+	}
 
-        carousel.skip = function(direction) {
-            if (!carousel.current_left) carousel.current_left = 0;
+        this.skip = function(direction) {
+            if (!this.current_left) this.current_left = 0;
 
             // calculate current_left
             if (direction == "next")
-                carousel.current_left -= carousel.options.shift;
+                this.current_left -= this.options.shift;
             if (direction == "prev")
-                carousel.current_left += carousel.options.shift;
+                this.current_left += this.options.shift;
 
-            if (carousel.current_left + (carousel.items_container_width) <= 0)
-		    carousel.current_left = 0;
+            if (this.current_left + (this.items_container_width) <= 0)
+		    this.current_left = 0;
 
 	    //reset to last page
-            if (carousel.current_left > 0){
-                var pages_count = Math.ceil( carousel.items_container_width / carousel.options.shift );
-		carousel.current_left = - (pages_count-1) * carousel.options.shift;
+            if (this.current_left > 0){
+                var pages_count = Math.ceil( this.items_container_width / this.options.shift );
+		this.current_left = - (pages_count-1) * this.options.shift;
             }
 
             // animation
-            carousel.carousel_transition(carousel.current_left);
+            this.carousel_transition(this.current_left);
         };
 
 
-        carousel.carousel_transition = function(position) {
-            carousel.items_container.stop().animate({
+        this.carousel_transition = function(position) {
+            this.items_container.stop().animate({
                 "left": position
             },
             {
-                "duration": carousel.options.transition_duration,
-                "easing": carousel.options.transition_easing
+                "duration": this.options.transition_duration,
+                "easing": this.options.transition_easing
             });
         };
 
         // auto adjust carousel constructor ------------------------------------
-        carousel.auto_adjust_carousel = function() {
+        this.auto_adjust_carousel = function() {
 
-            add_wrappers = function(){
+            //add_wrappers = function(){
                 // add class to scrolled div
-                carousel.items_container.addClass("row_of_slides");
+                this.items_container.addClass("row_of_slides");
 
                 // add wrapper 1
-                carousel.items_container.wrap('<div class="slide_holder_inner" />');
+                this.items_container.wrap('<div class="slide_holder_inner" />');
 
                 // add wrapper 2
-                carousel.items_container.parent(".slide_holder_inner").wrap('<div class="slide_holder" />');
-                carousel.carousel_wrapper = carousel.items_container.parent(".slide_holder_inner").parent();
+                this.items_container.parent(".slide_holder_inner").wrap('<div class="slide_holder" />');
+                this.carousel_wrapper = this.items_container.parent(".slide_holder_inner").parent();
 
                 // add prev/next buttons
-                carousel.carousel_wrapper.append('<div class="prev_button">&lt;</div>');
-                carousel.prev_button = carousel.carousel_wrapper.children(".prev_button");
+                this.carousel_wrapper.append('<div class="prev_button">&lt;</div>');
+                this.prev_button = this.carousel_wrapper.children(".prev_button");
 
-                carousel.carousel_wrapper.append('<div class="next_button">&gt;</div>');
-                carousel.next_button = carousel.carousel_wrapper.children(".next_button");
+                this.carousel_wrapper.append('<div class="next_button">&gt;</div>');
+                this.next_button = this.carousel_wrapper.children(".next_button");
 
-                carousel.items_count = carousel.items_container.children().length;                
-            };
+                this.items_count = this.items_container.children().length;                
+            //};
 
-            setup_items_and_wrappers = function(){
-                carousel.items_container.css('width', carousel.items_container_width);
-                carousel.items_container.children().css("width", carousel.item_width+"px");
-                carousel.items_container.children().addClass("slide");
-            };
-
-            add_wrappers();
-
-            carousel.item_width = Math.floor(carousel.items_container.parent().width() / carousel.options.number_slides_visible);
-            carousel.items_container_width = Math.floor(carousel.items_count * carousel.item_width);
+            this.item_width = Math.floor(this.items_container.parent().width() / this.options.number_slides_visible);
+            this.items_container_width = Math.floor(this.items_count * this.item_width);
                                              
-            setup_items_and_wrappers();
+            //setup_items_and_wrappers = function(){
+                this.items_container.css('width', this.items_container_width);
+                this.items_container.children().css("width", this.item_width+"px");
+                this.items_container.children().addClass("slide");
+            //};
 
-            carousel.options.shift =  carousel.options.scroll * carousel.item_width;
+            this.options.shift =  this.options.scroll * this.item_width;
 
+	    var carousel = this;
             // add events for buttons
-            carousel.next_button.click(function() {
+            this.next_button.click(function() {
                 carousel.skip('next');
             });
 
-            carousel.prev_button.click(function() {
+            this.prev_button.click(function() {
                 carousel.skip('prev');
             });
 
         } 
 
+	this.initialize();
+
+
     } // function agile_carousel
+
+    //$.auto_carousel = function(element, input_options) {
+    //    proto = $.gcarousel(element, input_options);
+    //    //$.extend( this, par );
+    //    prototype
+    //}
 
 })(jQuery);
