913046d63b24295edda5169803176df034186503
Wojciech Kruszewski <wojciech@oxos.pl>
2010-03-22 13:34:59 +0100

    Add jQuery function for pawkyAutoCarousel

--- pawkycarousel.js
+++ pawkycarousel.js
@@ -1,75 +1,82 @@
+jQuery.fn.pawkyCarousel = function(options) {
+	return this.each(function() {
+		new jQuery.pawkyCarousel(this, options);
+	});
+};
+
+jQuery.fn.pawkyAutoCarousel = function(options) {
+	return this.each(function() {
+		new jQuery.pawkyAutoCarousel(this, options);
+	});
+};
+
+
 /*
  * Basic Carousel.
  * Makes no assumptions about its content. Just moves a container inside a wrapper
  */
 
-	jQuery.fn.pawkyCarousel = function(options) {
-		return this.each(function() {
-			new jQuery.pawkyCarousel(this, options);
-		});
-	};
+jQuery.pawkyCarousel = function(element, input_options) {
 
-	jQuery.pawkyCarousel = function(element, input_options) {
+this.initialize = function(element, input_options) {
+		this.items_container = $(element);
 
-	this.initialize = function(element, input_options) {
-			this.items_container = $(element);
+		this.options = {
+			transition_duration: 1000,
+			transition_easing: "swing",
+			shift: 100
+		};
+		this.options.width = this.items_container.width();
 
-			this.options = {
-				transition_duration: 1000,
-				transition_easing: "swing",
-				shift: 100
-			};
-			this.options.width = this.items_container.width();
+		jQuery.extend(this.options, input_options);
 
-			jQuery.extend(this.options, input_options);
+}
 
+this.skip = function(direction) {
+	if (!this.current_left) this.current_left = 0;
+
+	// calculate current_left
+	if (direction == "next")
+		this.current_left -= this.options.shift;
+	if (direction == "prev")
+		this.current_left += this.options.shift;
+
+	if (this.current_left + (this.options.width) <= 0)
+	this.current_left = 0;
+
+	//reset to last page
+	if (this.current_left > 0){
+		var pages_count = Math.ceil( this.options.width / this.options.shift );
+		this.current_left = - (pages_count-1) * this.options.shift;
 	}
 
-	this.skip = function(direction) {
-		if (!this.current_left) this.current_left = 0;
+	// animation
+	this.carousel_transition(this.current_left);
+};
 
-		// calculate current_left
-		if (direction == "next")
-			this.current_left -= this.options.shift;
-		if (direction == "prev")
-			this.current_left += this.options.shift;
+this.prev = function() {
+		this.skip('prev');
+}
 
-		if (this.current_left + (this.options.width) <= 0)
-		this.current_left = 0;
-
-		//reset to last page
-		if (this.current_left > 0){
-			var pages_count = Math.ceil( this.options.width / this.options.shift );
-			this.current_left = - (pages_count-1) * this.options.shift;
-		}
-
-		// animation
-		this.carousel_transition(this.current_left);
-	};
-
-	this.prev = function() {
-			this.skip('prev');
-	}
-
-	this.next = function() {
-			this.skip('next');
-	}
+this.next = function() {
+		this.skip('next');
+}
 
 
-	this.carousel_transition = function(position) {
-		this.items_container.stop().animate({
-			"left": position
-		},
-		{
-			"duration": this.options.transition_duration,
-			"easing": this.options.transition_easing
-		});
-	};
+this.carousel_transition = function(position) {
+	this.items_container.stop().animate({
+		"left": position
+	},
+	{
+		"duration": this.options.transition_duration,
+		"easing": this.options.transition_easing
+	});
+};
 
-	this.initialize(element, input_options);
+this.initialize(element, input_options);
 
 
-	} // function agile_carousel
+}
 
 
 
@@ -80,7 +87,7 @@
  * Creates wrappers and controls and resizes items
  */
 
-jQuery.auto_carousel = function(element, input_options) {
+jQuery.pawkyAutoCarousel = function(element, input_options) {
 	var options = {
 		number_slides_visible: "1",
 		scroll: 1
@@ -88,19 +95,14 @@ jQuery.auto_carousel = function(element, input_options) {
 	jQuery.extend(options, input_options);
 
 	proto = new jQuery.pawkyCarousel(element, options);
-	//return proto;
 
 	acar = function() {
-		// auto adjust carousel constructor ------------------------------------
 
 		this.add_wrappers_and_controls = function() {
-			// add class to scrolled div
 			this.items_container.addClass("row_of_slides");
 
-			// add wrapper 1
 			this.items_container.wrap('<div class="slide_holder_inner" />');
 
-			// add wrapper 2
 			this.items_container.parent(".slide_holder_inner").wrap('<div class="slide_holder" />');
 			this.carousel_wrapper = this.items_container.parent(".slide_holder_inner").parent();
 
@@ -134,6 +136,8 @@ jQuery.auto_carousel = function(element, input_options) {
 			this.add_wrappers_and_controls();
 			this.setup_items_and_wrappers();
 		} 
+
+		this.auto_adjust_carousel();
 
 	}
 
